0cefa4d5524a552773e6b433684a0e16b4885870
eoneoff <eoneoff@gmail.com>
2018-02-07 13:47:41 +0200

    completed add income window

--- application/Organizer/Organizer/IncomeEditControl.xaml.cs
+++ application/Organizer/Organizer/IncomeEditControl.xaml.cs
@@ -24,12 +24,57 @@ namespace Organizer
         public IncomeEditControl()
         {
             InitializeComponent();
+            using (organizerEntities db = new organizerEntities())
+            {
+                var incomeSources = db.IncomeSource.ToList();
+                IncomeSourceSelector.DataContext = incomeSources;
+            }
         }
 
         private void IncomeSum_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             Regex number = new Regex(@"^\d*[.,]?\d{0,4}$");
             e.Handled = !number.IsMatch(e.Text);
+        }
+
+        private async void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Income income = (Income)DataContext;
+            if(String.IsNullOrEmpty(IncomeSourceSelector.Text))
+            {
+                MessageBox.Show("Введите или выберите источник дохода", "Внимание", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                using (organizerEntities db = new organizerEntities())
+                {
+                    if(!db.IncomeSource.Any(s=>s.Name==IncomeSourceSelector.Text))
+                    {
+                        if (MessageBox.Show($"Вы хотите задать новый источник дохода \"{IncomeSourceSelector.Text}\"?", "Вы уверены?", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            IncomeSource newSource = new IncomeSource() { Name = IncomeSourceSelector.Text };
+                            income.IncomeSource = newSource;
+                        }
+                        else return;
+                    }
+                    else
+                    {
+                        income.IncomeSource = (IncomeSource)IncomeSourceSelector.SelectedItem;
+                    }
+
+                    db.Entry(income).State = income.Id == 0 ?
+                    System.Data.Entity.EntityState.Added :
+                    System.Data.Entity.EntityState.Modified;
+
+                    Window.GetWindow(this).Close();
+                    await db.SaveChangesAsync();
+                }
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
         }
     }
 }
